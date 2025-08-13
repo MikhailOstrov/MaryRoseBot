710332baf3d6fdba5de5ff592023e8bcd9e39199
MikhailOstorv <pro100mihail7@gmail.com>
2025-08-14 00:56:53 +0300

    Fix?

--- utils/backend_requests.py
+++ utils/backend_requests.py
@@ -7,12 +7,11 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 AI_BACKEND_URL = os.getenv("AI_BACKEND_URL")
-INTERNAL_API_KEY = os.getenv("INTERNAL_API_KEY")
 
 async def send_audio_to_backend(wav_path: str):
     """Отправка аудио на бэк"""
     url = f"{AI_BACKEND_URL}/api/v1/internal/audio"
-    headers = {"X-Internal-Api-Key": INTERNAL_API_KEY}
+    headers = {"X-Internal-Api-Key": 'key'}
 
     async with httpx.AsyncClient() as client:
         with open(wav_path, "rb") as f:
@@ -25,7 +24,7 @@ async def send_audio_to_backend(wav_path: str):
 async def send_text_to_backend(text: str):
     """Отправка текста на бэк"""
     url = f"{AI_BACKEND_URL}/api/v1/internal/text"
-    headers = {"X-Internal-Api-Key": INTERNAL_API_KEY}
+    headers = {"X-Internal-Api-Key": 'key'}
 
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json={"text": text}, headers=headers, timeout=30.0)
