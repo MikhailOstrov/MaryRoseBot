b46132a8f224ea68a71c7567d7222642c428138c
MikhailOstorv <pro100mihail7@gmail.com>
2025-08-31 12:52:33 +0300

    Audio convertation + logic through download

--- handlers/search_handler.py
+++ handlers/search_handler.py
@@ -1,11 +1,13 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram import Router, F, Bot
+import os
 
 from states.kb_states import SearchKnowledge
 from services.kb_requests import get_info_from_kb
 from utils.llm_handler import response_for_searh
 from services.back_requests import send_audio_to_backend
+from utils.convert_audio import convert_audio_to_wav
 from config import logger
 
 router = Router()
@@ -28,14 +30,24 @@ async def process_add_audio(message: Message, state: FSMContext, bot: Bot):
     chat_id = message.chat.id
     file_id = message.voice.file_id if message.voice else message.audio.file_id
 
+    temp_dir = "temp"
+    os.makedirs(temp_dir, exist_ok=True)
+
+    # Создаем полный путь к временному OGG-файлу.
+    temp_ogg_path = os.path.join(temp_dir, f"{file_id}.ogg")
+
     try:
+        # 1. Получаем информацию о файле и скачиваем его
         file_info = await bot.get_file(file_id)
-        audio_stream = await bot.download_file(file_info.file_path)
-            
-        # Преобразуем объект _io.BytesIO в байты
-        audio_bytes = audio_stream.getvalue()
         
-        response_audio = await send_audio_to_backend(audio_bytes, chat_id)
+        # Скачиваем файл во временное место на диске
+        await bot.download_file(file_info.file_path, destination=temp_ogg_path)
+        logger.info(f"Аудиофайл скачан в {temp_ogg_path}")
+        
+        wav_path = convert_audio_to_wav(temp_ogg_path)
+        logger.info(f"Конвертация завершена. WAV-файл: {wav_path}")
+
+        response_audio = await send_audio_to_backend(wav_path, chat_id)
         
         search_text = await response_for_searh(response_audio)
         result = await get_info_from_kb(search_text, message.chat.id)
@@ -45,3 +57,7 @@ async def process_add_audio(message: Message, state: FSMContext, bot: Bot):
     except Exception as e:
         logger.error(f"Ошибка при обработке аудио: {e}")
         await message.reply("Произошла ошибка при обработке аудиофайла. Попробуйте еще раз.")
+
+    finally:
+        if os.path.exists(temp_ogg_path):
+            os.remove(temp_ogg_path)
